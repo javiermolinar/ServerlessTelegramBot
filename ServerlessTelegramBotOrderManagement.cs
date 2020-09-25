cd1648b853ef31bd6f67f602d865d5120d516582
javiermolinar <javiermolinar@live.com>
2020-09-25 14:37:29 +0200

    Added a new function to handle orders

---
+++ ServerlessTelegramBotOrderManagement.cs
@@ -0,0 +1,37 @@
+// Default URL for triggering event grid function in the local environment.
+// http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.EventGrid.Models;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Telegram.Bot;
+using Microsoft.Azure.EventGrid;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ServerlessTelegramBot
+{
+    public class ServerlessTelegramBotOrderManagement
+    {
+        private readonly ITelegramBotClient botClient;
+        private readonly IEventGridClient eGClient;
+
+        public ServerlessTelegramBotOrderManagement( ITelegramBotClient client, IEventGridClient eventGridClient){
+            botClient = client;
+            eGClient = eventGridClient;
+        }
+
+        [FunctionName("ServerlessTelegramBotOrderManagement")]
+        public async Task Run([EventGridTrigger]EventGridEvent eventGridEvent)
+        {
+               await botClient.SendTextMessageAsync(
+               chatId: new Telegram.Bot.Types.ChatId(eventGridEvent.Id),
+               text: $"{eventGridEvent.Data} esta en cocina");
+
+               Thread.Sleep(5000);
+
+               await botClient.SendTextMessageAsync(
+               chatId: new Telegram.Bot.Types.ChatId(eventGridEvent.Id),
+               text: $"{eventGridEvent.Data} esta en camino");               
+        }
+    }
+}
